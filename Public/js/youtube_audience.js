c1be816f04c60489232ac76780af81553c012f37
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2024-11-01 22:45:24 +0700

    Upload files

---
+++ Public/js/youtube_audience.js
@@ -0,0 +1,57 @@
+import DrawLineChart, {
+    randomNumberArray,
+    START_X,
+} from "./index.js"
+
+const canvas = document.getElementById("line-chart")
+
+const ctx = canvas.getContext('2d');
+
+const dlc = new DrawLineChart(
+    ctx,
+    ["Followers"],
+    [2000],
+    [START_X - 30],
+    ['rgb(75, 192, 192)'],
+    ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept'],
+    "",
+    localStorage.getItem("theme") === "dark-mode" ? "dark-mode" : "light-mode",
+    [...randomNumberArray(32, 60, 160), 158]
+)
+
+dlc.draw()
+
+const canvas2 = document.getElementById("line-chart-2")
+const ctx2 = canvas2.getContext("2d")
+
+const dlc2 = new DrawLineChart(
+    ctx2,
+    ["Audiences"],
+    [50],
+    [START_X - 30],
+    ["rgb(75, 192, 192)"],
+    ["May", "Jun", "Jul", "Aug", "Sep"],
+    "M",
+    localStorage.getItem("theme") === "dark-mode" ? "dark-mode" : "light-mode",
+    [...randomNumberArray(16, 50, 150), 117]
+)
+
+dlc2.draw()
+
+const colorTheme = localStorage.getItem("theme");
+
+if (colorTheme === "dark-mode") {
+    document.body.classList.add("dark-mode");
+} else if (colorTheme === "light-mode") {
+    document.body.classList.remove("dark-mode");
+}
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
+    if (document.body.classList.contains("dark-mode")) {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("theme", "light-mode");
+    } else {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark-mode");
+    }
+});
---
+++ Public/js/youtube_video.js
@@ -0,0 +1,39 @@
+import DrawLineChart, {
+    START_X,
+    randomNumberArray
+} from "./index.js"
+
+const lineChart = document.getElementById("line-chart")
+const ctx = lineChart.getContext("2d")
+
+const dlc = new DrawLineChart(
+    ctx,
+    [""],
+    [15],
+    [START_X - 25],
+    ['rgb(33, 102, 51)'],
+    ["Mon", "Tue", "Wed", "Thu", "May", "Fri", "Sat", "Sun"],
+    "",
+    localStorage.getItem("theme") === "dark-mode" ? "dark-mode" : "light-mode",
+    [...randomNumberArray(32, 40, 170), 153]
+)
+
+dlc.draw();
+
+const colorTheme = localStorage.getItem("theme");
+
+if (colorTheme === "dark-mode") {
+    document.body.classList.add("dark-mode");
+} else if (colorTheme === "light-mode") {
+    document.body.classList.remove("dark-mode");
+}
+
+document.getElementById("theme-toggle").addEventListener("click", () => {
+    if (document.body.classList.contains("dark-mode")) {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("theme", "light-mode");
+    } else {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark-mode");
+    }
+});
